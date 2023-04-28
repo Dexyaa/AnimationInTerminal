ef5d9b398f3a5d61a772d896fc93649bc8f7034e
Geodude <geolundgustuf@gmail.com>
2023-04-28 11:29:45 -0500

    changing the functionality of the file

--- app/src/main/java/animationinterminal/Animation.java
+++ app/src/main/java/animationinterminal/Animation.java
@@ -6,40 +6,42 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.OutputStreamWriter;
 import java.util.Date;
-import java.util.Scanner;
 public class Animation{
     private int frames;
-    private File animation = new File("src/resources/animation.txt");
+    private File animation = new File("C:\\Users\\Geo\\Desktop\\AnimationInTerminal\\app\\src\\main\\java\\animationinterminal\\animation.txt");
     public Animation(int frame){
         frames = frame;
         System.out.println(animation.getPath());
     }
+
+    //TODO: Make new read function
+    public void read
+
     public void animate(){
         String separator = System.lineSeparator();
         System.out.println("Reading from " + animation);
         try{
-            if(animation.canRead()){
-                try (BufferedReader reader = new BufferedReader(new FileReader(animation))) {
-                    while(reader.readLine() != null){
-                        BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
-                        long startTime = System.currentTimeMillis();
-                        long elaspedTime = 0L;
-                        while(!reader.readLine().equals(separator) && elaspedTime < (double) 1000/frames){
-                            String data = reader.readLine();
-                            log.write(data);
-                            elaspedTime = (new Date()).getTime() - startTime;
-                            //Thread.sleep(1000/frames);
-                        }
-                        System.out.flush();
-                        if(elaspedTime < 1000){
-                            Thread.sleep((double)1000/frames - elaspedTime);
-                        }
-                        System.out.println("\033[H\033[2J");
+            try (BufferedReader reader = new BufferedReader(new FileReader(animation))) {
+                String data = "";
+                while(data.equals(null)){
+                    BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
+                    long startTime = System.currentTimeMillis();
+                    long elaspedTime = 0L;
+                    while(!data.equals(separator) && elaspedTime < 1000/frames){
+                        data = reader.readLine();
+                        log.write(data);
+                        elaspedTime = (new Date()).getTime() - startTime;
+                            
                     }
+                    System.out.flush();
+                    if(elaspedTime < 1000){
+                        Thread.sleep(1000/frames - elaspedTime);
+                    }
+                    System.out.println("\033[H\033[2J");
                 }
-            } else {
-                System.out.println("This doesn't work...");
             }
+        } catch (NullPointerException f){
+            System.out.println(f + ". Something is wrong with your file");
         } catch (Exception e){
             System.out.println(e);
         }
